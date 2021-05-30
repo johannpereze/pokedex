75bf47975d530297ccc2fea481ab209f1897fd03
johannpereze <sebas.jsp@gmail.com>
2021-05-29 23:09:29 -0500

    Cambio la consulta a la api por un array de pikachu para no estar consultando la API con cada cambio

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,74 @@
+let stats = [
+  {
+      "base_stat": 35,
+      "effort": 0,
+      "stat": {
+          "name": "hp",
+          "url": "https://pokeapi.co/api/v2/stat/1/"
+      }
+  },
+  {
+      "base_stat": 55,
+      "effort": 0,
+      "stat": {
+          "name": "attack",
+          "url": "https://pokeapi.co/api/v2/stat/2/"
+      }
+  },
+  {
+      "base_stat": 40,
+      "effort": 0,
+      "stat": {
+          "name": "defense",
+          "url": "https://pokeapi.co/api/v2/stat/3/"
+      }
+  },
+  {
+      "base_stat": 50,
+      "effort": 0,
+      "stat": {
+          "name": "special-attack",
+          "url": "https://pokeapi.co/api/v2/stat/4/"
+      }
+  },
+  {
+      "base_stat": 50,
+      "effort": 0,
+      "stat": {
+          "name": "special-defense",
+          "url": "https://pokeapi.co/api/v2/stat/5/"
+      }
+  },
+  {
+      "base_stat": 90,
+      "effort": 2,
+      "stat": {
+          "name": "speed",
+          "url": "https://pokeapi.co/api/v2/stat/6/"
+      }
+  }
+]
+
 responsePokeApi = () => {
-  let pokemonID = document.getElementById('inputNumber').value;
-  
-  var settings = {
-    url: `https://pokeapi.co/api/v2/pokemon/${pokemonID}/`,
-    method: "GET",
-    timeout: 0,
-  };
-
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-
-    function writePokeInfo() {
+      function writePokeInfo() {
       document.getElementById(
         "sprite"
-      ).innerHTML = `<img src="${response.sprites.versions["generation-v"]["black-white"].animated.front_default}" alt="">`;
-      document.getElementById("name").innerHTML = `${response.forms[0].name}`;
-      document.getElementById("number").innerHTML = `${response.id}`;
+      ).innerHTML = `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif" alt="">`;
+      document.getElementById("name").innerHTML = `Pikachu`;
+      document.getElementById("number").innerHTML = `25`;
       document.getElementById(
         "type"
-      ).innerHTML = `${response.types[0].type.name}`;
-      for (let index = 0; index < response.stats.length; index++) {
+      ).innerHTML = `electric`;
+      for (let index = 0; index < stats.length; index++) {
         document.getElementById(
           `statName${index}`
-        ).innerHTML = `${response.stats[index].stat.name}`;
+        ).innerHTML = stats[index].stat.name;
         document.getElementById(
           `statValue${index}`
-        ).innerHTML = `${response.stats[index].base_stat}`;
+        ).innerHTML = stats[index].base_stat;
       }
     }
     writePokeInfo();
-  });
-};
+  }
+
+  responsePokeApi()
