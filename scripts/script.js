d7e7c74325059be02e98905911ba07c423c0eb4d
johannpereze <sebas.jsp@gmail.com>
2021-05-29 22:30:57 -0500

    Ya queda funcionando las solicitudes a la API por ID, Lo siguiente es darle los estilos a la pantalla de la pokedex

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,42 @@
+// let pokemonID = 150;
+
+// console.log(document.getElementById('inputNumber').value)
+
+// // function pokeID() {
+//   var pokemonID = document.getElementById('inputNumber').value;
+//   // return pokemonID
+//   // }
+
 responsePokeApi = () => {
-
-    var settings = {
-        "url": "https://pokeapi.co/api/v2/pokemon/25/",
-        "method": "GET",
-        "timeout": 0,
-      };
-      
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        
-    
-        function writePokeInfo(){
-            document.getElementById('sprite').innerHTML=`<img src="${response.sprites.versions["generation-v"]["black-white"].animated.front_default}" alt="">`;
-            }
-            writePokeInfo()
-        
-
-        
-      });
-
-}
-
-
+  var pokemonID = document.getElementById('inputNumber').value;
   
-  
+  var settings = {
+    url: `https://pokeapi.co/api/v2/pokemon/${pokemonID}/`,
+    method: "GET",
+    timeout: 0,
+  };
 
-  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+
+    function writePokeInfo() {
+      document.getElementById(
+        "sprite"
+      ).innerHTML = `<img src="${response.sprites.versions["generation-v"]["black-white"].animated.front_default}" alt="">`;
+      document.getElementById("name").innerHTML = `${response.forms[0].name}`;
+      document.getElementById("number").innerHTML = `${response.id}`;
+      document.getElementById(
+        "type"
+      ).innerHTML = `${response.types[0].type.name}`;
+      for (let index = 0; index < response.stats.length; index++) {
+        document.getElementById(
+          `statName${index}`
+        ).innerHTML = `${response.stats[index].stat.name}`;
+        document.getElementById(
+          `statValue${index}`
+        ).innerHTML = `${response.stats[index].base_stat}`;
+      }
+    }
+    writePokeInfo();
+  });
+};
