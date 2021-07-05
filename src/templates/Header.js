bfb8264b5c8dc1ea85017a41726f495ea857bf65
johannpereze <sebas.jsp@gmail.com>
2021-07-05 00:42:42 -0500

    Continuo con estilos css. Header a la mitad, se agregan sombras a los png, se centran los png, se hace la flecha de -atras-

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,15 +3,18 @@ import getHash from "../utils/getHash";
 
 const Header = async () => {
   const id = await getHash();
-  const pokemon = await getData(id); 
-  const headerImg = id === '/' ? './assets/images/pokedex.png' : pokemon.sprites.other["official-artwork"].front_default
-  const backArrow = id === '/' ? '' : '<span class="back-arrow">flecha</span>';
+  const pokemon = await getData(id);
+  const headerImg =
+    id === "/"
+      ? "./assets/images/pokedex.png"
+      : pokemon.sprites.other["official-artwork"].front_default;
+  const backArrow = id === "/" ? "" : '<img src="./assets/images/icons/arrow.svg" alt="back" class="back-arrow"></a> ';
   const view = `
   <div class="header-ball">
-    <div class="header-ball__artwork">
     <a href="#" class="back-arrow">${backArrow}</a>
-     <img src="${headerImg}">
-      <div class="header-ball__button">
+    <div class="header-ball__artwork">
+        <img class="header-ball__artwork--img" src="${headerImg}">
+        <div class="header-ball__button">
         <img src="${pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default}" alt="${pokemon.name}">
       </div>
     </div>
