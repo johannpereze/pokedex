936862e9e8dd03825481e282f3542bae82d8d620
johannpereze <sebas.jsp@gmail.com>
2021-07-05 11:03:22 -0500

    Avanzo en los estilos del home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import randomId from "../utils/randomId";
 
 const Home = async () => {
-    const Id = await randomId();
-    const view = `
+  const Id = await randomId();
+  const view = `
         <div class="social-menu">
             <a href="https://github.com/johannpereze/" target="_blank"><img src="./assets/images/icons/github.svg" alt="github"></a><a href="https://www.youtube.com/c/JohannPerezE"  target="_blank"><img src="./assets/images/icons/youtube.svg" alt="youtube"></a><a href="https://www.instagram.com/johannpereze/" target="_blank"><img src="./assets/images/icons/instagram.svg" alt="instagram"></a>
         </div>
-        <div><a href="#/${Id}/">Busca al azar</a></div>
-        <div>Busca por número</div>
+        <div class="home-menu">
+            <a href="#/${Id}/">
+                <div class="home-menu__card">
+                    <h2>Random Pokemon</h2>
+                    <h3>Discover a new one</h3>
+                </div>
+            </a>
+        </div>
     `;
-    return view;
-}
+  return view;
+};
 
 export default Home;
